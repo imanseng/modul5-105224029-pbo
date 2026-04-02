ba03fcb592771aa1a4efe53caa737a24877d595f
imanseng <imandwisatrio99@gmail.com>
2026-04-02 16:24:31 +0700

    kirim soal latihan 3

--- Latihan/src/Karyawan.java
+++ Latihan/src/Karyawan.java
@@ -3,6 +3,7 @@ public class Karyawan {
     private String idKaryawan; // Soal No 1
     private String nama; // Soal No 1
     private double gajiPokok; // Soal No 2
+    private String departemen; // Soal No 3 (katanya protected ganti private)
 
     // Soal No 1
     public Karyawan(String idKaryawan, String nama) {
@@ -42,5 +43,22 @@ public class Karyawan {
         } else {
             this.gajiPokok = gajiPokok;
         }
+    }
+
+    // Soal No 3 (getter sama setter kubuat jg)
+    public String getDepartemen() {
+        return departemen;
+    }
+
+    public void setDepartemen(String departemen) {
+        this.departemen = departemen;
+    }
+
+    // Soal No 3
+    public void tampilkanDataKaryawan() {
+        System.out.println("ID Karyawan: " + this.idKaryawan);
+        System.out.println("Nama: " + this.nama);
+        System.out.println("Gaji Pokok: " + this.gajiPokok);
+        System.out.println("Departemen: " + this.departemen);
     }
 }
