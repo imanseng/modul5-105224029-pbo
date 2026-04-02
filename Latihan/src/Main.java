6d412f73c90ad633c1857f292587e5fd9c133273
imanseng <imandwisatrio99@gmail.com>
2026-04-02 16:29:59 +0700

    kirim soal latihan no 4

---
+++ Latihan/src/Main.java
@@ -0,0 +1,10 @@
+// Soal no 4
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Karyawan karyawan = new Karyawan("K001", "Gio");
+        karyawan.setDepartemen("Artificial Intelligence");
+        karyawan.setGajiPokok(-9999999);
+        karyawan.setGajiPokok(500);
+        karyawan.tampilkanDataKaryawan();
+    }
+}
