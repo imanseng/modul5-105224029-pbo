201121bc1c099cc32d3ea995690850b1af77c0ed
imanseng <imandwisatrio99@gmail.com>
2026-04-02 16:21:01 +0700

    kirim soal latihan no 2

--- Latihan/src/Karyawan.java
+++ Latihan/src/Karyawan.java
@@ -2,6 +2,7 @@
 public class Karyawan {
     private String idKaryawan; // Soal No 1
     private String nama; // Soal No 1
+    private double gajiPokok; // Soal No 2
 
     // Soal No 1
     public Karyawan(String idKaryawan, String nama) {
@@ -27,5 +28,19 @@ public class Karyawan {
     // Soal No 1
     public void setNama(String nama) {
         this.nama = nama;
+    }
+
+        // Soal No 2
+    public double getGajiPokok() {
+        return gajiPokok;
+    }
+
+    // Soal No 2
+    public void setGajiPokok(double gajiPokok) {
+        if (gajiPokok < 0) {
+            System.out.println("Gaji tidak boleh kurang dari nol");
+        } else {
+            this.gajiPokok = gajiPokok;
+        }
     }
 }
